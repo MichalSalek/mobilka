9be535ee20f459794e2a25e74eec852113b7a2fc
Michał Sałek <contact@michalsalek.pl>
2025-01-20 19:36:27 +0100

    update

--- src/READONLY-shared-kernel/domain/admin/become_user.config.ts
+++
@@ -1,1 +0,0 @@
-export const BECOME_USER_SESSION_COOKIE_NAME = 'become_user_session'
--- src/READONLY-shared-kernel/domain/http/http.config.ts
+++ src/READONLY-shared-kernel/domain/http/http.config.ts
@@ -7,3 +7,10 @@ export type HTTPMethod = 'post' | 'get'
 export type HTTPStatus = 200 | 201 | 400 | 401 | 402 | 404 | 405 | 406 | 500 | 503
 
 export const API_VER = '/api/v1/'
+
+export const CUSTOM_HEADERS = {
+  authorization: 'authorization',
+  userMetadata : 'user-metadata',
+  becomeUser   : 'become-user'
+} as const
+
