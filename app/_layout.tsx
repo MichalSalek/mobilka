76bbc09c2b2b000ac130d4d4eb906c807b24d753
MichalSalek <contact@michalsalek.com>
2025-04-29 10:59:41 +0200

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,10 +9,22 @@ const styles = StyleSheet.create({
   },
 });
 
+// https://github.com/react-native-webview/react-native-webview/blob/1ddfe70521725c365cf8accf2a1bdf82eb4db80f/docs/Reference.md
+
 export default function RootLayout() {
 
   return (
     <WebView
+      // cacheMode // https://developer.android.com/reference/android/webkit/WebSettings.html#setCacheMode(int)
+      // overScrollMode={'never'} // https://developer.android.com/reference/android/view/View#setOverScrollMode(int)
+      // androidLayerType={'hardware'}
+
+      domStorageEnabled={true}
+      userAgent={true}
+      mixedContentMode={'compatibility'}
+      setBuiltInZoomControls={false}
+
+      useWebView2={true} //@TODO TEST
       style={styles.container}
       source={{uri: 'http://192.168.1.34/'}}
       originWhitelist={['*']}
